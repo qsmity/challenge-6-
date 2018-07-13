d31c2a77921aed68c978f201c3fc8616875683ab
qsmity <31673566+qsmity@users.noreply.github.com>
2018-07-13 14:17:18 -0400

    question 1
    question 1 -Question 1: Clean the room function: given an input of [1,2,4,591,392,391,2,5,10,2,1,1,1,20,20], make a function that organizes these into individual array that is ordered. For example answer(ArrayFromAbove) should return: [[1,1,1,1],[2,2,2], 4,5,10,[20,20], 391, 392,591]. Bonus: Make it so it organizes strings differently from number types. i.e. [1, "2", "3", 2] should return [[1,2], ["2", "3"]]

---
+++ challenge6.js
@@ -0,0 +1,62 @@
+var arraySort = [1,2,4,591,392,391,2,5,10,2,1,1,1,20,20];
+var strSort = [1,2,4,"591",392,"391",2,5,10,"2",1,1,1,"20",20];
+
+function subArray(arr){
+  //make a tmp array to make each sub array
+  let tmpArray = []; 
+  //make empty array to put sub array and individuals components
+  let result = []; 
+
+  //sort array
+  let sortArray = arr.sort((a,b)=> a-b);
+  console.log(sortArray);
+ 
+
+  for (i = 0; i < sortArray.length; i++) {
+    if (i === 0 || sortArray[i] === sortArray[i-1]) {
+      tmpArray.push(sortArray[i]);
+    } else if (sortArray[i] === sortArray[i+1] && sortArray[i] !== sortArray[i-1]) {
+          if (sortArray[i-1] !== sortArray[i-2]){
+            tmpArray.push(sortArray[i]);
+          } else {
+            result.push(tmpArray);
+            tmpArray = [];
+            tmpArray.push(sortArray[i]);
+          }
+     } else {
+          if (sortArray[i-1] !== sortArray[i-2]){
+          result.push(sortArray[i]);
+          } else {
+          result.push(tmpArray);
+          result.push(sortArray[i]);
+          tmpArray = [];
+          }
+      }
+  };
+  return result
+};
+
+function sepArray(arr){
+  let finalArray = []; 
+  let numArray = [];
+  let strArray = []; 
+
+  for (i = 0; i < arr.length; i++){
+    if (typeof arr[i] === 'string') {
+      strArray.push(arr[i]);
+    } else if (typeof arr[i] === 'number') {
+      numArray.push(arr[i]);
+    }
+  }
+  console.log(numArray, strArray);
+  let orderedArray = subArray(numArray);
+  console.log(orderedArray); 
+  finalArray.push(orderedArray, strArray);
+  console.log(finalArray); 
+};
+
+//checks if there are strings in the array, sorts them from #'s, sort numbered array into sub arrays, then add string array
+sepArray(strSort); 
+
+//just sorts all number arrays into sub arrays
+// console.log(subArray(arraySort)); 
